8de07c207c76bb4eb9541581cd888ca3715933d7
Nallana Sai Suraj <nallanasaisuraj@gmail.com>
2026-02-17 09:03:05 +0530

    Refactored UC3: Banner creation using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,21 +1,34 @@
 /**
  * OOPSBannerApp
- * UC2 - Display OOPS as ASCII banner using print statements
+ * UC3 - Display OOPS banner using String.join()
  *
  * @author Sai Suraj
- * @version 2.0
+ * @version 3.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" *****   *****   *****   ***** ");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *      ");
-        System.out.println("*     * *     * *****   *****  ");
-        System.out.println("*     * *     * *            * ");
-        System.out.println("*     * *     * *     * *    * ");
-        System.out.println(" *****   *****  *     *  ****  ");
+        System.out.println(String.join("",
+                " *****   ", " *****   ", " *****   ", " ***** "));
+
+        System.out.println(String.join("",
+                "*     * ", "*     * ", "*     * ", "*     *"));
+
+        System.out.println(String.join("",
+                "*     * ", "*     * ", "*     * ", "*      "));
+
+        System.out.println(String.join("",
+                "*     * ", "*     * ", "*****   ", " ***** "));
+
+        System.out.println(String.join("",
+                "*     * ", "*     * ", "*      ", "      *"));
+
+        System.out.println(String.join("",
+                "*     * ", "*     * ", "*     *", " *    *"));
+
+        System.out.println(String.join("",
+                " *****   ", " *****  ", "*     *", "  **** "));
 
     }
 }
