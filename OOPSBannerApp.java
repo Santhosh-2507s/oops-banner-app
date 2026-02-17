a585dd4f74dbb06f1e5c63fe23fe6a3dda48cba9
Nallana Sai Suraj <nallanasaisuraj@gmail.com>
2026-02-17 09:24:35 +0530

    Fixed UC3: Corrected banner alignment using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -9,13 +9,13 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(String.join(""," *****  "," *****  "," *****   ", " ***** "));
-        System.out.println(String.join("","*     * ", "*      * ", "*     * ", "*     *"));
-        System.out.println(String.join("","*     * ", "*      * ", "*     * ", "*      "));
-        System.out.println(String.join("","*     * ", "*      * ", "*****   ", " ***** "));
-        System.out.println(String.join("","*     * ", "*      * ", "*      ", "      *"));
-        System.out.println(String.join("","*     * ", "*      * ", "*      ", " *    *"));
-        System.out.println(String.join(""," *****   ", " *****  ", "*      ", "  **** "));
+        System.out.println(String.join(""," *****  "," *****  ","*****   ", "**** "));
+        System.out.println(String.join("","*     * ", "*     * ", "*    * ", "*    *"));
+        System.out.println(String.join("","*     * ", "*     * ", "*    * ", "*      "));
+        System.out.println(String.join("","*     * ", "*     * ", "*****   ", "****"));
+        System.out.println(String.join("","*     * ", "*     * ", "*      ", "      *"));
+        System.out.println(String.join("","*     * ", "*     * ", "*      ", " *    *"));
+        System.out.println(String.join(""," *****   ", "*****  ", "*      ", "  **** "));
 
     }
 }
